99e723a22e2da226d5807153010c07a76071e354
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-07-16 00:08:01 -0300

    criação da conexão do mysql

---
+++ dw_tools/dw_tools.py
@@ -0,0 +1,13 @@
+import sqlalchemy as sa
+
+
+def connect_mysql(
+    username: str,
+    password: str,
+    database: str,
+    server: str,
+    port: int
+) -> sa.engine.Connection:
+    url = f"mysql+pymysql://{username}:{password}@{server}:{port}/{database}"
+
+    return sa.create_engine(url=url).connect()
