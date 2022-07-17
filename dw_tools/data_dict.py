f759441b97a11122b385f99b5695d3ac3d5d4fc8
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-07-16 21:56:40 -0300

    criação do dicionário de dados para ser usado na fato

---
+++ dw_tools/data_dict.py
@@ -0,0 +1,352 @@
+data_dict = {
+    "influd": {
+        "DT_NOTIFIC": None,
+        "ID_MUNICIP": None,
+        "SEM_NOT": None,
+        "NU_ANO": None,
+        "SG_UF_NOT": None,
+        "DT_SIN_PRI": None,
+        "DT_NASC": None,
+        "NU_IDADE_N": None,
+        "CS_SEXO": {
+            "M": "MASCULINO",
+            "F": "FEMININO",
+            "I": "IGNORADO"
+        },
+        "CS_GESTANT": {
+            1: "1º TRIMESTRE",
+            2: "2º TRIMESTRE",
+            3: "3º TRIMESTRE",
+            4: "IDADE GESTACIONAL IGNORADA",
+            5: "NÃO",
+            6: "NÃO SE APLICA",
+            9: "IGNORADO"
+        },
+        "CS_RACA": {
+            1: "BRANCA",
+            2: "PRETA",
+            3: "AMARELA",
+            4: "PARDA",
+            5: "INDÍGENA",
+            9: "IGNORADO"
+        },
+        "CS_ESCOL_N": {
+            0: "ANALFABETO",
+            1: "FUNDAMENTAL (1-9 ANOS)",
+            2: "MÉDIO (1-3 ANOS)",
+            3: "SUPERIOR",
+            9: "IGNORADO",
+            10: "NÃO SE APLICA"
+        },
+        "SG_UF": None,
+        "ID_MN_RESI": None,
+        "ID_OCUPA_N": None,
+        "VACINA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "FEBRE": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "TOSSE": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "CALAFRIO": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "DISPNEIA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "GARGANTA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "ARTRALGIA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "MIALGIA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "CONJUNTIV": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "CORIZA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "DIARREIA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "OUTRO_SIN": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "OUTRO_DES": None,
+        "CARDIOPATI": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "PNEUMOPATI": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "RENAL": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "HEMOGLOBI": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "IMUNODEPRE": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "TABAGISMO": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "METABOLICA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "OUT_MORBI": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "MORB_DESC": None,
+        "HOSPITAL": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "DT_INTERNA": None,
+        "CO_UF_INTE": None,
+        "CO_MU_INTE": None,
+        "DT_PCR": None,
+        "PCR_AMOSTR": None,
+        "PCR_OUT": None,
+        "PCR_RES": None,
+        "PCR_ETIOL": None,
+        "PCR_TIPO_H": None,
+        "PCR_TIPO_N": None,
+        "DT_CULTURA": None,
+        "CULT_AMOST": None,
+        "CULT_OUT": None,
+        "CULT_RES": None,
+        "DT_HEMAGLU": None,
+        "HEMA_RES": None,
+        "HEMA_ETIOL": None,
+        "HEM_TIPO_H": None,
+        "HEM_TIPO_N": None,
+        "DT_RAIOX": None,
+        "RAIOX_RES": {
+            1: "NORMAL",
+            2: "INFILTRADO INTERSTICIAL",
+            3: "CONSOLIDACAO",
+            4: "MISTO",
+            5: "OUTRO",
+            6: "NÃO REALIZADO",
+            9: "IGNORADO"
+        },
+        "RAIOX_OUT": None,
+        "CLASSI_FIN": {
+            1: "SRAG POR INFLUENZA",
+            2: "SRAG POR OUTROS VÍRUS RESPIRATÓRIOS",
+            3: "SRAG POR POR OUTROS AGENTES ETIOLÓGICOS",
+            4: "SRAG NÃO ESPECIFICADO"
+        },
+        "CLASSI_OUT": None,
+        "CRITERIO": {
+            1: "LABORATORIAL",
+            2: "CLÍNICO - EPIDEMIOLÓGICO",
+            3: "CLÍNICO"
+        },
+        "TPAUTOCTO": None,
+        "DOENCA_TRA": None,
+        "EVOLUCAO": {
+            1: "RECEBEU ALTA POR CURA",
+            2: "EVOLUIU PARA ÓBITO",
+            9: "IGNORADO"
+        },
+        "DT_OBITO": None,
+        "DT_ENCERRA": None,
+        "DT_DIGITA": None,
+        "OBES_IMC": None,
+        "OUT_AMOST": None,
+        "DS_OAGEETI": None,
+        "DS_OUTMET": None,
+        "DS_OUTSUB": None,
+        "OUT_ANTIV": None,
+        "DT_COLETA": None,
+        "DT_ENTUTI": None,
+        "DT_ANTIVIR": None,
+        "DT_IFI": None,
+        "DT_OUTMET": None,
+        "DT_PCR_1": None,
+        "DT_SAIDUTI": None,
+        "RES_ADNO": {
+            1: "POSITIVO",
+            2: "NEGATIVO",
+            3: "INCONCLUSIVO",
+            4: "NÃO REALIZADO"
+        },
+        "AMOSTRA": {
+            1: "NÃO COLETOU",
+            2: "SECREÇÃO DE ORO E NASOFARINGE",
+            3: "TECIDO POST-MORTEM",
+            4: "LAVADO BROCO-ALVEOLAR",
+            5: "OUTRA",
+            9: "IGNORADO"
+        },
+        "HEPATICA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "NEUROLOGIC": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "OBESIDADE": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "PUERPERA": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "SIND_DOWN": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "RES_FLUA": {
+            1: "POSITIVO",
+            2: "NEGATIVO",
+            3: "INCONCLUSIVO",
+            4: "NÃO REALIZADO"
+        },
+        "RES_FLUB": {
+            1: "POSITIVO",
+            2: "NEGATIVO",
+            3: "INCONCLUSIVO",
+            4: "NÃO REALIZADO"
+        },
+        "UTI": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "IFI": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "PCR": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "RES_OUTRO": {
+            1: "POSITIVO",
+            2: "NEGATIVO",
+            3: "INCONCLUSIVO",
+            4: "IGNORADO"
+        },
+        "OUT_METODO": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "RES_PARA1": {
+            1: "POSITIVO",
+            2: "NEGATIVO",
+            3: "INCONCLUSIVO",
+            4: "NÃO REALIZADO"
+        },
+        "RES_PARA2": {
+            1: "POSITIVO",
+            2: "NEGATIVO",
+            3: "INCONCLUSIVO",
+            4: "NÃO REALIZADO"
+        },
+        "RES_PARA3": {
+            1: "POSITIVO",
+            2: "NEGATIVO",
+            3: "INCONCLUSIVO",
+            4: "NÃO REALIZADO"
+        },
+        "DESC_RESP": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "SATURACAO": {
+            1: "SIM",
+            2: "NÃO",
+            9: "IGNORADO"
+        },
+        "ST_TIPOFI": {
+            1: "FICHA ANTIGA",
+            2: "FICHA NOVA"
+        },
+        "TIPO_PCR": {
+            1: "CONVENCIONAL",
+            2: "EM TEMPO REAL"
+        },
+        "ANTIVIRAL": {
+            1: "NÃO",
+            2: "OSELTAMIVIR",
+            3: "ZANAMIVIR",
+            4: "OUTRO",
+            9: "IGNORADO"
+        },
+        "SUPORT_VEN": {
+            1: "NÃO USOU",
+            2: "SIM, INVASIVO",
+            3: "SIM, NÃO INVASIVO",
+            9: "IGNORADO"
+        },
+        "RES_VSR": None,
+        "RES_FLUASU": {
+            1: "INFLUENZA A(H1N1) PDM09",
+            2: "INFLUENZA A(H1) SAZONAL",
+            3: "INFLUENZA A(H3) SAZONAL",
+            4: "INFLUENZA A NÃO SUBTIPADO",
+            5: "INFLUENZA A/H3N2V",
+            6: "OUTRO SUBTIPO DE INFLUENZA A"
+        }
+    }
+}
